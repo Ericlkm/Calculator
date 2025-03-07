8104b1fcf2805185390ffb21f6f130bac522ea11
Ericlkm <gtprinceeric.el1@gmail.com>
2025-03-07 04:54:16 -0500

    added type feature

--- index.js
+++ index.js
@@ -4,23 +4,34 @@ function displayValue(val) {
   display.value += val;
   console.log(display.value);
 }
-
+let value = display.value;
 function calc() {
-  try {
+  if (display.value !== "") {
     display.value = eval(display.value);
-    console.log(typeof display.value);
-  } catch (err) {
+  } else {
     display.value = "Error";
     setTimeout(() => {
-      location.reload();
-    }, 2000);
+      display.value = "";
+    }, 1000);
   }
 }
 const clearBtn = document.getElementById("clear");
 
 function clear() {
   display.value = "";
-  console.log("clear");
 }
 
 clearBtn.addEventListener("click", clear);
+
+addEventListener("keyup", (e) => {
+  console.log(display.value);
+  if (e.key === "Enter" && display.value === "") {
+    display.value = "error";
+    setTimeout(() => {
+      display.value = "";
+    }, 1000);
+  }
+  if (e.key === "Enter") {
+    display.value = eval(display.value);
+  }
+});
